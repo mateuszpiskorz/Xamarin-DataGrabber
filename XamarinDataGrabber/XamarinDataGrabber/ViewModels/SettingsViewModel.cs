66f411c8baf5ef7b7519e2a68651385fbadeb735
mateuszpiskorz <piskorzm@hotmail.com>
2020-08-25 19:33:42 +0200

    Added TODO comments

--- XamarinDataGrabber/XamarinDataGrabber/ViewModels/SettingsViewModel.cs
+++ XamarinDataGrabber/XamarinDataGrabber/ViewModels/SettingsViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
+using Xamarin.Forms;
 using XamarinDataGrabber.Models;
 
 namespace XamarinDataGrabber.ViewModels
@@ -75,14 +76,38 @@ namespace XamarinDataGrabber.ViewModels
         }
         public ICommand SaveCommand { get; set; }
         public ICommand DefaultCommand { get; set; }
-        #endregion
+        #endregion 
+        
         public SettingsViewModel()
         {
+            //Setting initial values to fields
             _ipAddress = DefaultConfigParams.defaultIpAdress;
             _ipPort = DefaultConfigParams.defaultIpPort;
             _apiVersion = DefaultConfigParams.defaultApiVersion;
             _maxSamples = DefaultConfigParams.defaultMaxSamples;
             _sampleTime = DefaultConfigParams.defaultSampleTime;
+
+
+            //Creating Commands for View buttons
+            DefaultCommand = new Command(() => { this.SetDefaultSettings(); });
+            SaveCommand = new Command(() => { this.SaveSettings(); });
+        }
+
+        //Default Button method
+        private void SetDefaultSettings()
+        {
+            IpAddress = DefaultConfigParams.defaultIpAdress;
+            IpPort = DefaultConfigParams.defaultIpPort;
+            ApiVersion = DefaultConfigParams.defaultApiVersion;
+            MaxSamples = DefaultConfigParams.defaultMaxSamples;
+            SampleTime = DefaultConfigParams.defaultSampleTime;
+        }
+
+        //Save Button method
+        private void SaveSettings()
+        {
+            //TODO: Implement Saving Method
+            //TOTHINK: using MessagingCenter and sending Message to MainViewModel where Http request are made???
         }
     }
 }
